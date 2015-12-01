759921ed2316f9847c819a64cf921f53cf4a1a35
David Beeler <david.beeler@gmail.com>
2015-12-01 12:41:03 -0600

    Addition of api route

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require "bundler"
 Bundler.require
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :database => "chicago_recommendations"
+)
+
 get "/" do
   return "Test"
+end
+
+
+get "/api" do
+  # SELECT * FROM recommendations;
+  return Place.all.to_json
 end
